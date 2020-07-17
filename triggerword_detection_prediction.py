76536c10a3c8854a08bdd09d8d4a4b63a39398ac
Divya Bhanu <divyabhanu.althi@stupify.in>
2020-07-18 02:02:31 +0530

    Initial Commit

---
+++ triggerword_detection_prediction.py
@@ -0,0 +1,139 @@
+import numpy as np
+from pydub import AudioSegment
+import random
+import sys
+import io
+import os
+import glob
+import IPython
+from td_utils import *
+%matplotlib inline
+
+Tx = 5998 # The number of time steps input to the model from the spectrogram
+n_freq = 101 # Number of frequencies input to the model at each time step of the spectrogram
+Ty = 1496 # The number of time steps in the output of our model
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+import os
+from pydub import AudioSegment
+
+# Calculate and plot spectrogram for a wav audio file
+def graph_spectrogram(wav_file):
+    rate, data = get_wav_info(wav_file)
+    print(rate,data)
+    nfft = 200 # Length of each window segment
+    fs = 8000 # Sampling frequencies
+    noverlap = 120 # Overlap between windows
+    nchannels = data.ndim
+    if nchannels == 1:
+        pxx, freqs, bins, im = plt.specgram(data, nfft, fs, noverlap = noverlap)
+    elif nchannels == 2:
+        pxx, freqs, bins, im = plt.specgram(data[:,0], nfft, fs, noverlap = noverlap)
+    print(pxx.shape)
+    if(pxx.shape[1]<5998):
+      new_shape = (101,5998)
+      shape_diff = np.array(new_shape) - np.array(pxx.shape)
+      pxx=np.lib.pad(pxx, ((0,shape_diff[0]),(0,shape_diff[1])), 
+                                'constant', constant_values=(0))
+      print(pxx.shape)
+    print(pxx.shape)
+    return pxx[:,:5998]
+
+# Load a wav file
+def get_wav_info(wav_file):
+    rate, data = wavfile.read(wav_file)
+    return rate, data
+
+# Used to standardize volume of audio clip
+def match_target_amplitude(sound, target_dBFS):
+    change_in_dBFS = target_dBFS - sound.dBFS
+    return sound.apply_gain(change_in_dBFS)
+from keras.callbacks import ModelCheckpoint
+from keras.models import Model, load_model, Sequential
+from keras.layers import Dense, Activation, Dropout, Input, Masking, TimeDistributed, LSTM, Conv1D
+from keras.layers import GRU, Bidirectional, BatchNormalization, Reshape
+from keras.optimizers import Adam
+
+# GRADED FUNCTION: model
+
+def model(input_shape):
+    """
+    Function creating the model's graph in Keras.
+    
+    Argument:
+    input_shape -- shape of the model's input data (using Keras conventions)
+
+    Returns:
+    model -- Keras model instance
+    """
+    
+    X_input = Input(shape = input_shape)
+    
+    ### START CODE HERE ###
+    
+    # Step 1: CONV layer (≈4 lines)
+    X = Conv1D(196, kernel_size=15, strides=4)(X_input)                                 # CONV1D
+    X = BatchNormalization()(X)                                 # Batch normalization
+    X = Activation('relu')(X)                                 # ReLu activation
+    X = Dropout(0.1)(X)                                 # dropout (use 0.8)
+
+    # Step 2: First GRU Layer (≈4 lines)
+    X = GRU(units = 128, return_sequences = True)(X) # GRU (use 128 units and return the sequences)
+    X = Dropout(0.1)(X)                                 # dropout (use 0.8)
+    X = BatchNormalization()(X)                                 # Batch normalization
+    
+    # Step 3: Second GRU Layer (≈4 lines)
+    X = GRU(units = 128, return_sequences = True)(X)   # GRU (use 128 units and return the sequences)
+    X = Dropout(0.1)(X)                                 # dropout (use 0.8)
+    X = BatchNormalization()(X)                                  # Batch normalization
+    X = Dropout(0.1)(X)                                  # dropout (use 0.8)
+    
+    # Step 4: Time-distributed dense layer (≈1 line)
+    X = TimeDistributed(Dense(1, activation = "sigmoid"))(X) # time distributed  (sigmoid)
+
+    ### END CODE HERE ###
+
+    model = Model(inputs = X_input, outputs = X)
+    
+    return model  
+model = model(input_shape = (Tx, n_freq))
+model.summary()
+model = load_model('models/akiranewmodel')
+def detect_triggerword(filename):
+    plt.subplot(2, 1, 1)
+
+    x = graph_spectrogram(filename)
+    print(x.shape)
+    # the spectogram outputs (freqs, Tx) and we want (Tx, freqs) to input into the model
+    x  = x.swapaxes(0,1)
+    x = np.expand_dims(x, axis=0)
+    predictions = model.predict(x)
+    
+    plt.subplot(2, 1, 2)
+    plt.plot(predictions[0,:,0])
+    plt.ylabel('probability')
+    plt.show()
+    return predictions
+chime_file = "/Keras-Trigger-Word/audio_examples/chime.wav"
+def chime_on_activate(filename, predictions, threshold):
+    audio_clip = AudioSegment.from_wav(filename)
+    chime = AudioSegment.from_wav(chime_file)
+    Ty = predictions.shape[1]
+    # Step 1: Initialize the number of consecutive output steps to 0
+    consecutive_timesteps = 0
+    # Step 2: Loop over the output steps in the y
+    for i in range(Ty):
+        # Step 3: Increment consecutive output steps
+        consecutive_timesteps += 1
+        # Step 4: If prediction is higher than the threshold and more than 75 consecutive output steps have passed
+        if predictions[0,i,0] > threshold and consecutive_timesteps > 75:
+            # Step 5: Superpose audio and background using pydub
+            audio_clip = audio_clip.overlay(chime, position = ((i / Ty) * audio_clip.duration_seconds)*1000)
+            # Step 6: Reset consecutive output steps to 0
+            consecutive_timesteps = 0
+        
+    audio_clip.export("chime_output.wav", format='wav')
+filename = "Keras-Trigger-Word/raw_data/dev/test_akira1.wav"
+prediction = detect_triggerword(filename)
+chime_on_activate(filename, prediction, 0.5)
+IPython.display.Audio("./chime_output.wav")
